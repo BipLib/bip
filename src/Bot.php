dd3ebdf8dff2d54316c133ae7c60ecfc47c05e7b
Sepehr <sepehr7160@gmail.com>
2022-08-26 00:28:54 +0430

    replace setStage with setStartStage add stage property implement setStartStage and run method

--- src/Bot.php
+++ src/Bot.php
@@ -18,18 +18,35 @@ use Bip\Database\Database;
 class Bot
 {
     private Config $config;
+    private Stage $stage;
+
     public function __construct(Config $config)
     {
         $this->config = $config;
-        // config validation 
+        $config->validate(['token']);
+
     }
 
-    public function setStage(Stage $stage)
+    /**
+     * set start stage, it will be run when user isn't in a stage.
+     * @param Stage $stage
+     */
+    public function setStartStage(Stage $stage)
     {
+        $this->stage = $stage;
     }
 
     public function setDatabase(Database $database)
     {
+    }
+
+    /**
+     * run the bot.
+     */
+    public function run()
+    {
+        $this->stage->_config = $this->config;
+        $this->stage->controller();
     }
 
 }
