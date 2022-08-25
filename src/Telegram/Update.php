a49612c648d55661634c012a1e2557cbc4f611b3
Sepehr <sepehr7160@gmail.com>
2022-08-25 14:59:15 +0430

    add init , asArray and asObject methods

--- src/Telegram/Update.php
+++ src/Telegram/Update.php
@@ -11,7 +11,48 @@
 namespace Bip\Telegram;
 
 
+use function file_get_contents;
+use function json_decode;
+
 class Update
 {
+    private static mixed $update;
+
+    /**
+     * constructor is private to prevent making instance of Update.
+     */
+    private function __construct(){}
+
+    /**
+     * initialize update.
+     * @param null $associative
+     */
+    private static function init($associative = null){
+        if (empty(self::$update))
+            self::$update = json_decode(file_get_contents('php://input'),$associative);
+    }
+
+    /**
+     * get update as Array.
+     * @return array|null
+     */
+    public static function asArray():array|null
+    {
+        self::init(true);
+        return (array) self::$update;
+    }
+
+    /**
+     * get update as Object.
+     * @return object|null
+     */
+    public static function asObject():object|null
+    {
+        self::init();
+        return self::$update;
+    }
+    //public function asBundle(): UpdateBundle|null
+
+
 
 }
