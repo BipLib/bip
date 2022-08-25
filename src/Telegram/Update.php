25d3211f24ab07e6592ac0a03a398d0e29b6e353
Sepehr <sepehr7160@gmail.com>
2022-08-25 10:55:16 +0430

    initial commit

---
+++ src/Telegram/Update.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * This file is part of the Bip Library.
+ *
+ *  (c) Sepehr Soheili <sepehr0617@gmail.com>
+ *
+ *  For the full copyright and license information, please view the
+ *   LICENSE file that was distributed with this source code.
+ */
+
+namespace Bip\Telegram;
+
+
+class Update
+{
+
+}
