949476268b151e21e069ca411102eb61ad9425c6
Sepehr <sepehr7160@gmail.com>
2022-08-26 00:32:09 +0430

    add call and controller method add Config $_config property

--- src/App/Stage.php
+++ src/App/Stage.php
@@ -13,5 +13,21 @@ namespace Bip\App;
 
 abstract class Stage
 {
+    /**
+     * automatically assigned.
+     * @var Config
+     */
+    public Config $_config;
+    /**
+     * call a telegram api method.
+     * @param string $method
+     * @param array $params
+     * @return mixed
+     */
+    public abstract function call(string $method , array $params = []): mixed;
 
+    /**
+     * in every run of the Stage, controller automatically called.
+     */
+    public abstract function controller();
 }
