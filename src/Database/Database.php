654710563a704b9e2ddec424752a82d46fd6f27d
Sepehr <sepehr7160@gmail.com>
2022-08-29 18:16:10 +0430

    add document. replace getStage method with gatStages.

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -15,9 +15,23 @@ use Bip\App\Stage;
 
 interface Database
 {
+    /**
+     * insert user. if the user exists, returns false, on success returns true.
+     * @param Stage $stage
+     * @return bool
+     */
     public function insertUser(Stage $stage): bool;
 
-    public function getStage(): Stage|bool;
+    /**
+     * get the user stages. returns false on failure.
+     * @return object|bool
+     */
+    public function getStages(): object|bool;
 
+    /**
+     * update the stage. returns true on success and false on failure.
+     * @param Stage $stage
+     * @return bool
+     */
     public function updateStage(Stage $stage): bool;
 }
