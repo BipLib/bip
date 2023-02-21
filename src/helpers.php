cdcd12f78fd7ce8434b263d65476cde7af7a632f
Sepehr <sepehr7160@gmail.com>
2023-02-22 00:51:01 +0330

    initial commit

---
+++ src/helpers.php
@@ -0,0 +1,58 @@
+<?php
+// main functions
+if (! function_exists('msg')) {
+    /**
+     * short function for sending message.
+     * @param string $text
+     * @return void
+     */
+    function msg(string $text) : void
+        {
+            \Bip\Telegram\Telegram::sendMessage(
+                chat_id : \Bip\Telegram\Update::get()->message->chat->id,
+                text : $text,
+            );
+        }
+}
+if (! function_exists('update')){
+    /**
+     * get update object.
+     * @return object
+     */
+    function update(): object
+    {
+        return \Bip\Telegram\Update::get();
+    }
+
+}
+// Bot functions
+if (! function_exists('bindNode')) {
+    /**
+     * @param string $node
+     * @return void
+     */
+    function bindNode(string $node): void
+    {
+        \Bip\Bot::bindNode($node);
+    }
+}
+if (! function_exists('changeStage')) {
+    /**
+     * @param string $newStage
+     * @return void
+     */
+    function changeStage(string $newStage): void
+    {
+        \Bip\Bot::changeStage($newStage);
+    }
+}
+if (! function_exists('route')) {
+    /**
+     * @param string $node
+     * @return \Bip\App\RouteRule
+     */
+    function route(string $node): \Bip\App\RouteRule
+    {
+        return \Bip\Bot::route($node);
+    }
+}
