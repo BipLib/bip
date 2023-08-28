6c016a2379d9b49e9e720184bc45ff84b9413ba3
Sepehr <87861266+sepsoh@users.noreply.github.com>
2023-08-28 23:19:07 +0330

    Update config.php

--- bots/NotePad/config.php
+++ bots/NotePad/config.php
@@ -6,5 +6,5 @@ use Bip\Database\LazyJsonDatabase;
 Config::add([
     'token'     => 'your bot api key',
     'admins'    => [12345678,123456789],
-    'database'  => new LazyJsonDatabase('database.json.db'),
+    'database'  => LazyJsonDatabase::init('database.json.db'),,
 ]);
