7ebc25ebca741c47a04f732ca8eb25bd0e79fbb8
Sepehr <sepehr7160@gmail.com>
2023-09-06 01:03:16 +0330

    new structure of database config

--- bots/NotePad/config.php
+++ bots/NotePad/config.php
@@ -2,9 +2,14 @@
 
 use Bip\App\Config;
 use Bip\Database\LazyJsonDatabase;
-
+//hi
 Config::add([
     'token'     => 'your bot api key',
     'admins'    => [12345678,123456789],
-    'database'  => new LazyJsonDatabase('database.json.db'),
+    'database'  => [
+        'driver' => LazyJsonDatabase::class,
+        'args' => [
+            'file'=>'database.json'
+        ]
+    ]
 ]);
