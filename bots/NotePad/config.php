af9d03ea844f15a57cfaa8f192a845dddf8b047f
sepehr <sepehr0617@gmail.com>
2024-05-20 00:00:53 +0330

    update to new version bip

--- bots/NotePad/config.php
+++ bots/NotePad/config.php
@@ -1,10 +1,15 @@
 <?php
 
 use Bip\App\Config;
-use Bip\Database\LazyJsonDatabase;
+use Bip\Database\LazyJson;
 
 Config::add([
     'token'     => 'your bot api key',
     'admins'    => [12345678,123456789],
-    'database'  => new LazyJsonDatabase('database.json.db'),
+    'database'=>[
+        'driver' => LazyJson::class,
+        'args'=>[
+        'file' => 'database.json'
+        ]
+    ]
 ]);
--- bots/NotePad/index.php
+++ bots/NotePad/index.php
@@ -6,7 +6,4 @@ require 'config.php';
 use Bip\Bot;
 use Bots\NotePad\Stages\StartStage;
 
-
-
-Bot::init(new StartStage());
-Bot::run();
+Bot::run(new StartStage());
